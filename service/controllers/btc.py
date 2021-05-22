bbc4f0948163d26cb796d24f0007564b97d7a464
Djcharles26 <jlaraanaya@gmail.com>
2021-05-22 00:30:37 -0500

    Updated btc controller

--- service/controllers/btc.py
+++ service/controllers/btc.py
@@ -1,3 +1,4 @@
+# %%
 import numpy as np
 import pandas as pd
 import matplotlib
@@ -6,7 +7,9 @@ from matplotlib import pyplot as plt
 import pandas_datareader as web
 import datetime as dt
 
-from sklearn.preprocessing import MinMaxScaler
+from itertools import chain
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import MinMaxScaler 
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
 
@@ -15,6 +18,7 @@ against_currency = "USD"
 
 start = dt.datetime(2016,1,1)
 end = dt.datetime.now()
+end = end.today() - dt.timedelta(days=1)
 
 data = web.DataReader(f"{crypto_currency}-{against_currency}", "yahoo", start, end)
 
@@ -22,11 +26,13 @@ data = web.DataReader(f"{crypto_currency}-{against_currency}", "yahoo", start, e
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 
-prediction_days = 5
-future_day = 5
+# %%
+prediction_days = 90
+future_day = 0
 
 x_train, y_train = [], []
 
+#x : 60 -> 1964
 for x in range(prediction_days, len(scaled_data) - future_day):
     x_train.append(scaled_data[x-prediction_days: x, 0])
     y_train.append(scaled_data[x + future_day,0])
@@ -52,19 +58,24 @@ model.compile(
     optimizer="adam", loss="mean_squared_error"
 )
 
-model.fit(x_train, y_train, epochs=25, batch_size=32)
+model.fit(x_train, y_train, epochs=50, batch_size=32)
 
+# %%
 #Testing the model
 
 test_start = dt.datetime(2020, 1, 1)
 test_end = dt.datetime.now()
+test_end = test_end.today() - dt.timedelta(days=1)
 
 test_data = web.DataReader(f"{crypto_currency}-{against_currency}", "yahoo", test_start, test_end)
 actual_prices = test_data["Close"].values
 
 total_dataset = pd.concat((data["Close"], test_data["Close"]), axis=0)
 
-model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+# 2467 - 503 1964 - 60 1904 
+# (1964 + 503) - 503 - 60 = 1906
+model_inputs = total_dataset[len(data) - prediction_days:].values
+
 model_inputs = model_inputs.reshape(-1,1)
 model_inputs = scaler.fit_transform(model_inputs)
 
@@ -79,19 +90,31 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 prediction_prices = model.predict(x_test)
 prediction_prices = scaler.inverse_transform(prediction_prices)
 
-print(actual_prices)
-print("=============================================")
-print(prediction_prices)
+#%%
+print(len(x_test))
+print(len(prediction_prices))
 
-plt.plot(actual_prices, color="black", label="Actual Prices")
-plt.plot(prediction_prices, color="green", label="Predicted Prices")
+from sklearn.metrics import r2_score
+y_predict = [x[0] for x in prediction_prices]
+
+score = r2_score(actual_prices, y_predict)
+
+print(score)
+
+date_list = [test_end - dt.timedelta(days=x) for x in range(len(actual_prices))]
+date_list = date_list[::-1]
+
+plt.plot(date_list, actual_prices,  'r--', color="red", label="Actual Prices")
+plt.plot(date_list, prediction_prices, color="green", label="Predicted Prices")
 plt.title(f"{crypto_currency} price prediction")
 plt.xlabel("Time")
 plt.ylabel("Price")
 plt.legend(loc="upper left")
 
 # Predict Next Day
-print(len(model_inputs))
+# 1904 + 1 1905 - 60 1845 : 1905
+print( len(model_inputs) + 1 - prediction_days)
+print(len(model_inputs) + 1)
 real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs) + 1, 0]]
 real_data = np.array(real_data)
 real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
@@ -100,6 +123,7 @@ real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
 prediction = model.predict(real_data)
 prediction = scaler.inverse_transform(prediction)
 print(prediction)
-plt.scatter(len(actual_prices) + 1, prediction)
+plt.scatter([date_list[len(date_list) - 1] + dt.timedelta(days=1)], prediction)
 
 plt.show()
+# %%
