ca50a63cf07a3ac2b4ca4af872bd369880b6e066
Djcharles26 <jlaraanaya@gmail.com>
2021-05-25 01:03:59 -0500

    Added traceback print exc in routes

--- service/routes/btc.py
+++ service/routes/btc.py
@@ -1,3 +1,4 @@
+import traceback
 import ctypes
 import json
 import random
@@ -5,7 +6,6 @@ import re
 from cerver import *
 from cerver.http import *
 from cerver.headers import *
-
 from service.errors import *
 from service import runtime, btc
 from service.controllers.btc import *
@@ -17,4 +17,14 @@ def top_level_handler (http_receive, request):
     try:
         http_send_response(http_receive,HTTP_STATUS_OK, {"msg": "BTC Works!"})
     except:
+        btc_error_send_response(BTC_ERROR_SERVER_ERROR, http_receive)
+
+#GET /api/btc/prediction
+@ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.c_void_p)
+def prediction_handler (http_receive, request):
+    try:
+        prediction = predict_next_day()
+        http_send_response(http_receive, HTTP_STATUS_OK, prediction)
+    except:
+        traceback.print_exc()
         btc_error_send_response(BTC_ERROR_SERVER_ERROR, http_receive)
